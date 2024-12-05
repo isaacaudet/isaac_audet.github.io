3b933c303ffbe8936cecd0abdb8d02fb254a65f8
IsaacAudet <isaac.audet@gmail.com>
2024-12-04 18:27:12 -0800

    move

---
+++ _astro/hoisted.fyIz0fhm.js
@@ -0,0 +1,9 @@
+import { a as t } from './hoisted.XRwbKKUJ.js'
+import './hoisted.8ps2ZCTu.js'
+const o = localStorage.getItem('animations') === 'true'
+o &&
+	t(
+		'.title',
+		{ y: [80, 0], opacity: [0, 1] },
+		{ duration: 2.5, opacity: { duration: 3 }, offset: [0, 0.55, 0.75] }
+	)
